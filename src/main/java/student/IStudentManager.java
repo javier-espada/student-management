5ddd7f1c29db1fafc849ff70db3bc7c976a0d880
Guzmán González de Riancho <guzmangriancho@gmail.com>
2025-12-17 11:40:59 +0100

    feat: added istudentmanager

---
+++ src/main/java/student/IStudentManager.java
@@ -0,0 +1,16 @@
+package student;
+
+import exception.*;
+import java.util.List;
+import java.util.Map;
+
+public interface IStudentManager {
+
+    void addStudent(String name, List<Double> grades) throws DuplicateStudentException;
+
+    void recordGrade(String studentName, double grade) throws StudentNotFoundException, InvalidGradeException;
+
+    String getStudentDetails(String studentName) throws StudentNotFoundException, NoGradesException;
+
+    Map<String, Double> getHighPerformingStudents(double minGrade);
+}
