65eb3bbcaab133b13fad7151644946195dcd93ea
Miguel Domenech <miguel.domenech@qaracter.com>
2025-12-17 13:52:15 +0100

    test: created TestStudentManager class, addStudent test

---
+++ src/test/java/test/TestStudentManager.java
@@ -0,0 +1,21 @@
+package test;
+
+import exception.DuplicateStudentException;
+import exception.NoGradesException;
+import org.junit.jupiter.api.Test;
+import student.Student;
+import student.StudentManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestStudentManager {
+    @Test
+    public void testAddStudent() throws NoGradesException {
+        final String name1 = "Juan";
+        final String name2 = "Maria";
+        StudentManager studentManager = new StudentManager();
+        assertDoesNotThrow(()->studentManager.addStudent(name1));
+        assertDoesNotThrow(()->studentManager.addStudent(name2));
+        assertThrows(DuplicateStudentException.class, ()->studentManager.addStudent(name1));
+    }
+}
