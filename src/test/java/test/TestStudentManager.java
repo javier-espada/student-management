2060e4a01234ab57a9984acb1216d84be627cfd6
Miguel Domenech <miguel.domenech@qaracter.com>
2025-12-18 10:31:17 +0100

    test: added tests for recordGrade and getStudentDetails

--- src/test/java/test/TestStudentManager.java
+++ src/test/java/test/TestStudentManager.java
@@ -2,6 +2,7 @@ package test;
 
 import exception.DuplicateStudentException;
 import exception.NoGradesException;
+import exception.StudentNotFoundException;
 import org.junit.jupiter.api.Test;
 import student.Student;
 import student.StudentManager;
@@ -17,5 +18,27 @@ public class TestStudentManager {
         assertDoesNotThrow(()->studentManager.addStudent(name1));
         assertDoesNotThrow(()->studentManager.addStudent(name2));
         assertThrows(DuplicateStudentException.class, ()->studentManager.addStudent(name1));
+    }
+
+    @Test
+    public void testRecordGrade() {
+        final String name1 = "Juan";
+        StudentManager studentManager = new StudentManager();
+        assertDoesNotThrow(()->studentManager.addStudent(name1));
+        assertThrows(NoGradesException.class, ()->studentManager.getStudentDetails(name1));
+        assertDoesNotThrow(()->studentManager.recordGrade(name1, 3.0));
+        System.out.println(assertDoesNotThrow(()->studentManager.getStudentDetails(name1)));
+    }
+
+    @Test
+    public void testGetStudentDetails() {
+        final String name1 = "Juan";
+        StudentManager studentManager = new StudentManager();
+        assertThrows(StudentNotFoundException.class, ()->studentManager.getStudentDetails(name1));
+        assertDoesNotThrow(()->studentManager.addStudent(name1));
+        assertThrows(StudentNotFoundException.class, ()->studentManager.getStudentDetails("Paco"));
+        assertThrows(NoGradesException.class, ()->studentManager.getStudentDetails(name1));
+        assertDoesNotThrow(()->studentManager.recordGrade(name1, 3.0));
+        System.out.println(assertDoesNotThrow(()->studentManager.getStudentDetails(name1)));
     }
 }
